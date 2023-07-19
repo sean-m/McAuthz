c18fdd5d0bb210f74968324e6e336aa840cbe14c
Sean McArdle <sean@mcardletech.com>
2023-07-19 08:21:14 -0700

    Removed unused parameter from attribute.

--- McAuthz/McAuthorizeAttribute.cs
+++ McAuthz/McAuthorizeAttribute.cs
@@ -8,16 +8,7 @@ namespace McAuthz {
 
     public class McAuthorizeAttribute : AuthorizeAttribute, IAuthorizeData {
         public McAuthorizeAttribute() { }
-        public McAuthorizeAttribute(string route) {
-            Route = route;
-        }
-
-        /// <summary>
-        /// The route associated with the given authorization policy. This allows
-        /// targetting specific policies per web api, mvc or razor pages route.
-        /// </summary>
-        public string? Route { get; set; }
-
-        public new string Policy { get => Globals.McPolicy; set => _ = value; }
+        
+        public new string? Policy { get => Globals.McPolicy; set => _ = value; }
     }
 }
