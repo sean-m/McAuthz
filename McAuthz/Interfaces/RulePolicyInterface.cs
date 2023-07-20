0d1d78ba50bded991bfdb1e37b44b9e18d448832
Sean McArdle <sean@mcardletech.com>
2023-07-20 14:29:32 -0700

    Doesn't need the interface here anymore.

--- McAuthz/Interfaces/RulePolicyInterface.cs
+++ McAuthz/Interfaces/RulePolicyInterface.cs
@@ -3,8 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace McAuthz.Policy {
-    public interface RulePolicy : IExpressionRule {
+namespace McAuthz.Interfaces {
+    public interface RulePolicy {
+        public string TargetType { get; set; }
+        public string Route { get; set; }
+
         bool EvaluateRules(object inputs);
         bool EvaluateRules(IEnumerable<object> inputs);
     }
