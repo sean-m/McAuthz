7c8f82a388298930abe60f200d873a9221578e60
Sean McArdle <sean@mcardletech.com>
2023-07-25 09:07:43 -0700

    This makes more sense in the Policy directory.

--- McAuthz/Policy/RulePolicyBase.cs
+++ McAuthz/Policy/RulePolicyBase.cs
@@ -5,15 +5,18 @@ using System.Linq.Expressions;
 using System.Linq;
 using System.Text;
 
-namespace McAuthz.Interfaces {
-    public class RulePolicyBase : IExpressionRule {
+namespace McAuthz.Policy
+{
+    public class RulePolicyBase : IExpressionRule
+    {
 
         public new string TargetType { get; set; }
         public string Route { get; set; }
         string IExpressionRule.TargetType { get => TargetType; set => TargetType = value; }
 
         internal List<IExpressionRule> policyRules = new List<IExpressionRule>();
-        public Expression<Func<T, bool>>? GetExpression<T>() {
+        public Expression<Func<T, bool>>? GetExpression<T>()
+        {
             return PredicateExpressionPolicyExtensions.CombineAnd(
                 policyRules.Select(x => x.GetExpression<T>() ?? PredicateBuilder.False<T>()));
         }
