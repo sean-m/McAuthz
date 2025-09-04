ec038d909f30aed0d845eef948d5612d7ce04471
Sean McArdle <sean@mcardletech.com>
2025-09-04 16:52:55 -0700

    Initial implementation of a policy which can mask values returned to a requestor.

---
+++ McAuthz.Tests/PolicyTests/MaskPolicyTest.cs
@@ -0,0 +1,80 @@
+﻿using McAuthz.Policy;
+using System.Dynamic;
+using System.Reflection;
+
+namespace McAuthz.Tests.PolicyTests;
+
+[TestFixture]
+public class MaskPolicyTest {
+
+    IEnumerable<Adventurer> Adventurers { get; set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        Adventurers = SMM.CsvFileReader.GetRecords<Adventurer>("./TestData/adventurers.csv").ToList();
+    }
+
+    [Test]
+    public void MaskPolicy_ShouldIncludeOnlySpecifiedProperties()
+    {
+        // Arrange
+        var adventurer = new Adventurer
+        {
+            Name = "Aragorn",
+            Title = "King",
+            Renown = "Legendary",
+            PrimaryClass = "ranger",
+            SecondaryClass = "fighter",
+            Alignment = "Lawful Good",
+            Gold = 777,
+            Hp = 80,
+            Constitution = 10,
+            Strength = 15,
+            Intelligence = 12,
+            Agility = 14,
+            Speed = 10
+        };
+
+        var maskProperties = new[] { "Name", "Title", "Gold" };
+        var maskPolicy = new MaskPolicy<Adventurer>(maskProperties);
+
+        // Act
+        var masked = maskPolicy.ApplyMask(adventurer);
+
+        Assert.That(adventurer.Name, Is.EqualTo(masked.Name));
+        Assert.That(adventurer.Title, Is.EqualTo(masked.Title));
+        Assert.That(adventurer.Gold, Is.EqualTo(masked.Gold));
+        Assert.IsNull(masked.Renown);
+    }
+
+    [Test]
+    public void MaskPolicy_ShouldIgnoreNonexistentProperties()
+    {
+        // Arrange
+        var adventurer = Adventurers.First();
+        var maskPolicy = new MaskPolicy<Adventurer>(new[] { "Name", "NonexistentProperty" });
+
+        dynamic masked = new ExpandoObject();
+        masked = maskPolicy.ApplyMask(adventurer);
+
+        // Assert
+        var dict = ToDictionary(masked);
+        Assert.IsNotNull(dict, "Masked result should be a dictionary");
+        Assert.IsTrue(dict.ContainsKey("Name"));
+        Assert.IsFalse(dict.ContainsKey("NonexistentProperty"));
+    }
+
+    internal Dictionary<string, object> ToDictionary<T>(T source)
+    {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+
+        var dict = new Dictionary<string, object>();
+        foreach (PropertyInfo prop in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance))
+        {
+            dict[prop.Name] = prop.GetValue(source, null);
+        }
+        return dict;
+    }
+}
---
+++ McAuthz/Policy/MaskPolicy.cs
@@ -0,0 +1,78 @@
+﻿using McAuthz.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace McAuthz.Policy {
+
+    /// <summary>
+    /// A policy that masks properties of a model based on a list of property names.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MaskPolicy: RulePolicyBase, RulePolicy {
+
+        public MaskPolicy() { }
+
+        public MaskPolicy(string typeName)
+        {
+            TargetType = typeName;
+        }
+
+        public MaskPolicy(Type type)
+        {
+            TargetType = type.Name;
+        }
+
+        public MaskPolicy(string typeName, params string[] properties)
+        {
+            TargetType = typeName;
+            ((List<string>)PropertyList).AddRange(properties);
+        }
+
+        public MaskPolicy(Type type, params string[] properties)
+        {
+            TargetType = type.Name;
+            ((List<string>)PropertyList).AddRange(properties);
+        }
+
+        public IEnumerable<string> PropertyList { get; set; } = new List<string>();
+
+        public object MaskModel(object model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MaskPolicy<T>: RulePolicyBase, RulePolicy {
+
+        public MaskPolicy() {
+            TargetType = typeof(T).Name;
+        }
+
+        public MaskPolicy(params string[] properties)
+        {
+            TargetType = typeof(T).Name;
+            ((List<string>)PropertyList).AddRange(properties);
+        }
+
+        public IEnumerable<string> PropertyList { get; set; } = new List<string>();
+
+        /// <summary>
+        /// Applies the mask to the given model, returning a new instance of T with only the specified properties.
+        /// All non-sepcified properties will be set to their default values.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public T ApplyMask(T model)
+        {
+            var props = PropertyList.ToArray();
+            var maskExpr = DynamicTypeRegistry.DynamicSelector<T>(props, true);
+            var mask = maskExpr.Compile();
+            var masked = mask(model);
+
+            return masked;
+        }
+    }
+}
